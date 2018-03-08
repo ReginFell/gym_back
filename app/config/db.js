2346a2ec9863ee0adb89fe232695260a1200a1d1
ReginFell@gmail.com <SoulReaver1>
2018-03-08 22:29:29 +0200

    test

--- app/config/db.js
+++ app/config/db.js
@@ -1,9 +1,5 @@
-const aws = require('aws-sdk');
-
-let db_user = process.env.MONGO_DB_USER;
-let db_password = process.env.MONGO_DB_PASSWORD;
-
+console.log(process.env.MONGODB_URI);
 
 module.exports = {
-    DB: "mongodb://" + db_user + ":" + db_password + "@ds259258.mlab.com:59258/heroku_ldtgb4dm"
+    DB: process.env.MONGODB_URI
 };
